bd6d68eefa5ca5b16933a266dc07a0f548c4c7b5
Ryan Beales <ryanbeales@gmail.com>
2025-07-27 09:05:21 -0700

    Update zfs maintenence script to new pool name

--- crobasaurusrex-admin/zfs-snapshot-cleanup-script.sh
+++ crobasaurusrex-admin/zfs-snapshot-cleanup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Remove the top 10 largest snapshots
-for i in $(zfs list -t snapshot -S used | head | tail -10 | cut -f1 -d\  ); do zfs destroy $i; done
+# Remove the top 10 largest snapshots - don't do this for now, we've just migrated disks
+#for i in $(zfs list -t snapshot -S used | head | tail -10 | cut -f1 -d\  ); do zfs destroy $i; done
 
 # Scrub
-sudo zpool scrub stashpool
+sudo zpool scrub newpool
